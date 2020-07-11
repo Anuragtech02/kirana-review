d1cfce427876a37729c97b6c4bd961dd3da29797
Anuragtech02 <apal895@gmail.com>
2020-07-11 11:26:14 +0530

    Added bg to app container and added heading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./App.module.css";
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className={styles.container}>
+      <h1>Kirana Review Feedbox</h1>
+    </div>
+  );
 };
 
 export default App;
