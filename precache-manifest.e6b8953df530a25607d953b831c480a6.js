4c629515f190cb8cc73a6dbba0d7cd8da412625f
Anuragtech02 <apal895@gmail.com>
2020-07-11 11:41:10 +0530

    Updates

---
+++ precache-manifest.e6b8953df530a25607d953b831c480a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fb29d9bd6556935961876adbeac063",
+    "url": "/kirana-review/index.html"
+  },
+  {
+    "revision": "a6f86e95ce7239bd4bd3",
+    "url": "/kirana-review/static/css/main.682678c3.chunk.css"
+  },
+  {
+    "revision": "05c3b5db0254d3cd7ef0",
+    "url": "/kirana-review/static/js/2.675e6756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kirana-review/static/js/2.675e6756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f86e95ce7239bd4bd3",
+    "url": "/kirana-review/static/js/main.cb185cdc.chunk.js"
+  },
+  {
+    "revision": "2b9847006ee7b06f4af7",
+    "url": "/kirana-review/static/js/runtime-main.6ce25472.js"
+  },
+  {
+    "revision": "f1565ac25de02b9295dccc2da13004ab",
+    "url": "/kirana-review/static/media/grocer-bg.f1565ac2.png"
+  }
+]);
