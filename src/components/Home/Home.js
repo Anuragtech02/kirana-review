ec8fe60584311630c83f15f9bd43d949f2381707
Anuragtech02 <apal895@gmail.com>
2020-07-17 16:36:53 +0530

    Added fontawesome cdn and few icons added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Home.module.css";
 import { Search } from "@material-ui/icons";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 export const Home = () => {
   return (
@@ -8,19 +9,22 @@ export const Home = () => {
       <div className={styles.heading}>
         <h1>Hey, we review Kirana Products</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <div className={styles.search}>
-          <div className={styles.title}>
-            <h4>Grocery</h4>
-          </div>
-          <div className={styles.input}>
-            <input type="text" placeholder="Searh your products from here" />
-          </div>
-          <div className={styles.btn}>
-            <button>
-              <Search /> Search
-            </button>
-          </div>
+      </div>
+      <div className={styles.search}>
+        <div className={styles.title}>
+          <h4>Grocery</h4>
         </div>
+        <div className={styles.input}>
+          <input type="text" placeholder="Searh your products from here" />
+        </div>
+        <div className={styles.btn}>
+          <button>
+            <Search /> Search
+          </button>
+        </div>
+      </div>
+      <div className={styles.scrollIcon}>
+        <i class="fas fa-mouse-alt"></i>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import classNames from "classnames";
 
 const Navbar = () => {
   return (
@@ -14,17 +15,26 @@ const Navbar = () => {
       </div>
       <ul className={styles.navLinks}>
         <li className={styles.navLink}>
-          <Link to="/" className={styles.noDecoration}>
+          <Link
+            to="/"
+            className={classNames(styles.noDecoration, styles.colorBlack)}
+          >
             Home
           </Link>
         </li>
         <li className={styles.navLink}>
-          <Link to="/about" className={styles.noDecoration}>
+          <Link
+            to="/about"
+            className={classNames(styles.noDecoration, styles.colorBlack)}
+          >
             About
           </Link>
         </li>
         <li className={styles.navLink}>
-          <Link to="/offer" className={styles.noDecoration}>
+          <Link
+            to="/offer"
+            className={classNames(styles.noDecoration, styles.colorBlack)}
+          >
             Offer
           </Link>
         </li>
