e2884a9427c726a950ef488cefa61f6bf599c40a
Anuragtech02 <apal895@gmail.com>
2020-07-17 18:48:47 +0530

    Updates

---
+++ precache-manifest.f26e603e419301edc684269dabdf7b4a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc37aa7633e46513837a61fac9e8d552",
+    "url": "/kirana-review/index.html"
+  },
+  {
+    "revision": "41ac63b01db8eb0991eb",
+    "url": "/kirana-review/static/css/2.fe979495.chunk.css"
+  },
+  {
+    "revision": "081db75a6b4b9662e6cc",
+    "url": "/kirana-review/static/css/main.209cc050.chunk.css"
+  },
+  {
+    "revision": "41ac63b01db8eb0991eb",
+    "url": "/kirana-review/static/js/2.23ae9c57.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/kirana-review/static/js/2.23ae9c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "081db75a6b4b9662e6cc",
+    "url": "/kirana-review/static/js/main.6231e663.chunk.js"
+  },
+  {
+    "revision": "2b9847006ee7b06f4af7",
+    "url": "/kirana-review/static/js/runtime-main.6ce25472.js"
+  },
+  {
+    "revision": "f1565ac25de02b9295dccc2da13004ab",
+    "url": "/kirana-review/static/media/grocer-bg.f1565ac2.png"
+  },
+  {
+    "revision": "2f8285b13bef950f843cb4147666af6e",
+    "url": "/kirana-review/static/media/image1.2f8285b1.png"
+  },
+  {
+    "revision": "1f7a4c9ea0ba5a216bc7af1f60d044e0",
+    "url": "/kirana-review/static/media/image2.1f7a4c9e.png"
+  },
+  {
+    "revision": "90d3534e1ad62a8b8a977f1290e61e9f",
+    "url": "/kirana-review/static/media/image3.90d3534e.png"
+  },
+  {
+    "revision": "2f8285b13bef950f843cb4147666af6e",
+    "url": "/kirana-review/static/media/image4.2f8285b1.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/kirana-review/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/kirana-review/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/kirana-review/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/kirana-review/static/media/slick.f97e3bbf.svg"
+  }
+]);
