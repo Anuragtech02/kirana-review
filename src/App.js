aac0f7fce3e418a585a048725d1ee4da312f2254
Mayank1403 <mayank1403@gmail.com>
2020-08-28 21:47:54 +0530

    Added Search Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Appbar, Navbar, About, Home ,Review,Categories, CategorizedProduct} from "./components";
+import {
+  Appbar,
+  Navbar,
+  About,
+  Home,
+  Review,
+  Categories,
+  CategorizedProduct,
+  Search,
+} from "./components";
 import styles from "./App.module.css";
-import {products} from "./Shared/Products";
+import { products } from "./Shared/Products";
 import { useLocation } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup, { SignupBase } from "./components/Signup/Signup";
+import SearchContext, { SearchContextProvider } from "./Context/searchContext";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -16,8 +26,6 @@ function ScrollToTop() {
 
   return null;
 }
-  
-  
 
 const App = () => {
   function setBackground() {
@@ -33,43 +41,59 @@ const App = () => {
       document.getElementById("navbar").style.boxShadow = "none";
     }
   }
- 
+
   useEffect(() => {
     function watchScroll() {
       window.addEventListener("scroll", setBackground);
-
     }
     watchScroll();
-   
+
     return () => {
       window.removeEventListener("scroll", setBackground);
     };
   }, []);
 
-   const ReviewWithId = ({match}) =>{
-     return <Review product ={products.filter((product)=>product.id === parseInt(match.params.id,10))[0]} 
-     products={products} /> ;
-   }
-   const productWithName = ({match})=>{
-     return <CategorizedProduct products = {products.filter((product)=> product.category === match.params.name)}/>
-   }
+  const ReviewWithId = ({ match }) => {
+    return (
+      <Review
+        product={
+          products.filter(
+            (product) => product.id === parseInt(match.params.id, 10)
+          )[0]
+        }
+        products={products}
+      />
+    );
+  };
+  const productWithName = ({ match }) => {
+    return (
+      <CategorizedProduct
+        products={products.filter(
+          (product) => product.category === match.params.name
+        )}
+      />
+    );
+  };
   return (
-    <Router>
-      <ScrollToTop/>
-      <div className={styles.navbar} id="navbar">
-        <Navbar />
-      </div>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/signupbase" component={SignupBase} />
-        <Route path="/Review/:id" exact component={ReviewWithId}/>
-        <Route path="/categories/:name" exact component={productWithName}/>
-        <Route path="/categories" component={Categories}/>
-      </Switch>
-    </Router>
+    <SearchContextProvider>
+      <Router>
+        <ScrollToTop />
+        <div className={styles.navbar} id="navbar">
+          <Navbar />
+        </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/signupbase" component={SignupBase} />
+          <Route path="/Review/:id" exact component={ReviewWithId} />
+          <Route path="/categories/:name" exact component={productWithName} />
+          <Route path="/search/:keyword" exact component={Search} />
+          <Route path="/categories" component={Categories} />
+        </Switch>
+      </Router>
+    </SearchContextProvider>
   );
 };
 
